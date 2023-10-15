b3b5dc4120b8a8bf48c87ffb747f7ea1fd47aa2c
Tangcuyu <Tangcuyu@users.noreply.github.com>
2023-10-15 13:08:08 +0000

    build storybook

--- docs/stencil-storybook-token-demo/stencil-storybook-token-demo.esm.js
+++ docs/stencil-storybook-token-demo/stencil-storybook-token-demo.esm.js
@@ -1,1 +1,1 @@
-import{b as a}from"./p-1cfe09be.js";import{p}from"./p-5a059910.js";p().then(p=>a([["p-80d36286",[[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}]]]],p));
+import{b as a}from"./p-1cfe09be.js";import{p as e}from"./p-5a059910.js";e().then(e=>a([["p-f8e37cf7",[[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}]]]],e));
