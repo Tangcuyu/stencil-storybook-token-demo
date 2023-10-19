ae5122282817a859b97671b408c7a9e1c4402e55
Tangcuyu <Tangcuyu@users.noreply.github.com>
2023-10-19 06:09:34 +0000

    build storybook

--- docs/stencil-storybook-token-demo/stencil-storybook-token-demo.esm.js
+++ docs/stencil-storybook-token-demo/stencil-storybook-token-demo.esm.js
@@ -1,1 +1,1 @@
-import{b as a}from"./p-22cdc34b.js";import{p as e}from"./p-e50baedd.js";e().then(e=>a([["p-957448be",[[1,"my-page"],[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}],[1,"my-card",{to:[1],label:[1],bgshadow:[4],target:[1],rel:[1],styles:[1]}]]]],e));
+import{b as a}from"./p-22cdc34b.js";import{p as e}from"./p-e50baedd.js";e().then(e=>a([["p-ea0d992c",[[1,"my-page"],[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}],[1,"my-card",{to:[1],label:[1],bgshadow:[4],target:[1],rel:[1],styles:[1]}]]]],e));
