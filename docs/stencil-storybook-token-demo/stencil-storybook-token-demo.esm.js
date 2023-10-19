83b5e7474c2769e9ca2be420921e625770361a1e
Tangcuyu <joysmshr@126.com>
2023-10-19 07:11:39 +0000

    Deploying to gh-pages from @ Tangcuyu/stencil-storybook-token-demo@87d267e92c75dbeaebd2299b09e6177203fcfb00 🚀

--- docs/stencil-storybook-token-demo/stencil-storybook-token-demo.esm.js
+++ docs/stencil-storybook-token-demo/stencil-storybook-token-demo.esm.js
@@ -1,1 +1,1 @@
-import{b as a}from"./p-22cdc34b.js";import{p as e}from"./p-e50baedd.js";e().then(e=>a([["p-6b023645",[[1,"my-page"],[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}],[1,"my-card",{to:[1],label:[1],bgshadow:[4],target:[1],rel:[1],styles:[1]}]]]],e));
+import{b as a}from"./p-22cdc34b.js";import{p as e}from"./p-e50baedd.js";e().then(e=>a([["p-3543746c",[[1,"my-page"],[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}],[1,"my-card",{to:[1],label:[1],bgshadow:[4],target:[1],rel:[1],styles:[1]}]]]],e));
