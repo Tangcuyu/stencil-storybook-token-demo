d03c8fb33e3afb7dc682a56cb91aa9efb3e16ed5
Tangcuyu <joysmshr@126.com>
2023-10-19 15:19:22 +0800

    feat():add two cards

--- src/components/my-page/my-page.tsx
+++ src/components/my-page/my-page.tsx
@@ -12,9 +12,15 @@ export class MyPage {
   render() {
     return (
       <Host>
-        <my-card to="http://">
+        <my-card class="mb">
           <div class="card-header">
             <span>Card name</span>
+            <my-button size="large" variant="tertiary" label="选项按钮" />
+          </div>
+        </my-card>
+        <my-card>
+          <div class="card-header">
+            <span>Card B</span>
             <my-button size="large" variant="tertiary" label="选项按钮" />
           </div>
         </my-card>
